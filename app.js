de94acd746192d5619845853ce8d7dccbf311e1e
hyeondev <hyeondev@gmail.com>
2015-11-19 22:19:21 +0900

    test

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var http = require('http');
 var request = require("request");
+var url  = require('url');
 
 http.createServer(function(req,res){
+ var url_parts = url.parse(req.url, true);
+ var query = url_parts.query;
+
+ console.log(query.addr);
+
  request({
-   uri: "https://kr.api.pvp.net/api/lol/kr/v1.4/summoner/by-name/우끼키끼?api_key=ee43eef2-b533-4c93-b010-6f99d46c8ae9",
+   uri: query.addr,
  }, function(error, response, body) {
-   res.writeHead(200);
-   res.write(body);
-   res.end();
-   console.log(body);
+   if(error){
+     console.log(error);
+   }
+   else {
+     res.writeHead(200);
+     res.write(body);
+     res.end();
+     console.log(body);
+   }
  });
 
 }).listen(8080);
